16f3356d5dbe8c36d153b72036676edad487c525
sonqo <stratoskrp@gmail.com>
2023-09-12 01:07:22 +0300

    Switched queries to use TopBonds table, added rated only bonds

--- 0-thesis/information-asymmetry/query-information-asymmetry.sql
+++ 0-thesis/information-asymmetry/query-information-asymmetry.sql
@@ -1,8 +1,8 @@
 -- TOP BONDS
 SELECT
 	*,
-	RetailVolume / (InstitutionalVolume + RetailVolume) AS VolumeFraction,
-	RetailTrades / (InstitutionalTrades + RetailTrades) AS TradesFraction
+	1.0 * RetailVolume / (InstitutionalVolume + RetailVolume) AS VolumeFraction,
+	1.0 * RetailTrades / (InstitutionalTrades + RetailTrades) AS TradesFraction
 FROM (
 	SELECT
 		EOMONTH(A.TrdExctnDt) AS TrdExctnDtEOM,
@@ -14,36 +14,8 @@ FROM (
 		COUNT(DISTINCT (CASE WHEN EntrdVolQt < 250000 THEN A.CusipId END)) AS RetailCusips
 	FROM
 		Trace_filteredWithRatings A
-	INNER JOIN (
-		SELECT
-			CusipId,
-			TrdExctnDtEOM
-		FROM (
-			SELECT
-				*,
-				DENSE_RANK() OVER (PARTITION BY IssuerId, TrdExctnDtEOM ORDER BY Volume DESC) AS VolumeRanking
-			FROM (
-				SELECT
-					IssuerId,
-					CusipId,
-					EOMONTH(TrdExctnDt) AS TrdExctnDtEOM,
-					SUM(EntrdVolQt) AS Volume
-				FROM
-					Trace_filteredWithRatings
-				WHERE
-					RatingNum <> 0
-					AND EntrdVolQt >= 500000 -- institunional
-					AND PrincipalAmt IS NOT NULL
-					AND TrdExctnDt <= EOMONTH(TrdExctnDt) AND TrdExctnDt > DATEADD(DAY, -5, EOMONTH(TrdExctnDt))
-				GROUP BY
-					IssuerId,
-					CusipId,
-					EOMONTH(TrdExctnDt)
-			) A
-		) B
-		WHERE
-			VolumeRanking <= 3
-	) B ON A.CusipId = B.CusipId AND EOMONTH(A.TrdExctnDt) = B.TrdExctnDtEOM
+	INNER JOIN
+		BondReturns_topBonds B ON A.CusipId = B.CusipId AND EOMONTH(A.TrdExctnDt) = B.TrdExctnDtEOM
 	GROUP BY
 		EOMONTH(A.TrdExctnDt)
 ) A
@@ -53,8 +25,8 @@ ORDER BY
 -- NON-TOP BONDS
 SELECT
 	*,
-	RetailVolume / (InstitutionalVolume + RetailVolume) AS VolumeFraction,
-	RetailTrades / (InstitutionalTrades + RetailTrades) AS TradesFraction
+	1.0 * RetailVolume / (InstitutionalVolume + RetailVolume) AS VolumeFraction,
+	1.0 * RetailTrades/ (InstitutionalTrades + RetailTrades) AS TradesFraction
 FROM (
 	SELECT
 		EOMONTH(A.TrdExctnDt) AS TrdExctnDtEOM,
@@ -66,67 +38,11 @@ FROM (
 		COUNT(DISTINCT (CASE WHEN EntrdVolQt < 250000 THEN A.CusipId END)) AS RetailCusips
 	FROM
 		Trace_filteredWithRatings A
-	INNER JOIN (
-
-		SELECT
-			CusipId,
-			TrdExctnDtEOM
-		FROM (
-			SELECT
-				*,
-				DENSE_RANK() OVER (PARTITION BY IssuerId, TrdExctnDtEOM ORDER BY Volume DESC) AS VolumeRanking
-			FROM (
-				SELECT
-					IssuerId,
-					CusipId,
-					EOMONTH(TrdExctnDt) AS TrdExctnDtEOM,
-					SUM(EntrdVolQt) AS Volume
-				FROM
-					Trace_filteredWithRatings
-				WHERE
-					RatingNum <> 0
-					AND EntrdVolQt >= 500000 -- institunional
-					AND PrincipalAmt IS NOT NULL
-					AND TrdExctnDt <= EOMONTH(TrdExctnDt) AND TrdExctnDt > DATEADD(DAY, -5, EOMONTH(TrdExctnDt))
-				GROUP BY
-					IssuerId,
-					CusipId,
-					EOMONTH(TrdExctnDt)
-			) A
-		) B
-		WHERE
-			VolumeRanking > 3
-
-		UNION
-
-		SELECT
-			CusipId,
-			TrdExctnDtEOM
-		FROM (
-			SELECT
-				*,
-				NULL AS VolumeRanking
-			FROM (
-				SELECT
-					IssuerId,
-					CusipId,
-					EOMONTH(TrdExctnDt) AS TrdExctnDtEOM,
-					SUM(EntrdVolQt) AS Volume
-				FROM
-					Trace_filteredWithRatings
-				WHERE
-					RatingNum <> 0
-					AND EntrdVolQt < 500000
-					AND PrincipalAmt IS NOT NULL
-					AND TrdExctnDt <= EOMONTH(TrdExctnDt) AND TrdExctnDt > DATEADD(DAY, -5, EOMONTH(TrdExctnDt))
-				GROUP BY
-					IssuerId,
-					CusipId,
-					EOMONTH(TrdExctnDt)
-			) A
-		) B
-
-	) B ON A.CusipId = B.CusipId AND EOMONTH(A.TrdExctnDt) = B.TrdExctnDtEOM
+	LEFT JOIN 
+		BondReturns_topBonds B ON A.CusipId = B.CusipId AND EOMONTH(A.TrdExctnDt) = B.TrdExctnDtEOM
+	WHERE
+		A.RatingNum <> 0
+		AND B.CusipId IS NULL
 	GROUP BY
 		EOMONTH(A.TrdExctnDt)
 ) A
