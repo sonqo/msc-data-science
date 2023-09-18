c1dc5f7c7fa017a4713f2df57861cc8f10ac6612
sonqo <stratoskrp@gmail.com>
2023-09-18 03:57:22 +0300

    Added consecutive month counter on TopBonds table

--- 0-thesis/db-config/tables/aux-bondReturns-topBonds.sql
+++ 0-thesis/db-config/tables/aux-bondReturns-topBonds.sql
@@ -1,33 +1,43 @@
 DROP TABLE IF EXISTS [dbo].[BondReturns_topBonds]
 
 SELECT
-	CusipId,
-	IssuerId,
-	TrdExctnDtEOM
+	*,
+	ROW_NUMBER() OVER (PARTITION BY CusipId, IssuerId, DateRanking ORDER BY TrdExctnDtEOM) as ConsecutiveMonths
 INTO
 	[dbo].[BondReturns_topBonds]
 FROM (
 	SELECT
 		*,
-		DENSE_RANK() OVER (PARTITION BY IssuerId, TrdExctnDtEOM ORDER BY Volume DESC) AS VolumeRanking
+		DATEPART(YEAR, TrdExctnDtEOM) * 12 + DATEPART(MONTH, TrdExctnDtEOM) - ROW_NUMBER() OVER (PARTITION BY CusipId, IssuerId ORDER BY TrdExctnDtEOM) AS DateRanking
 	FROM (
 		SELECT
-			IssuerId,
 			CusipId,
-			EOMONTH(TrdExctnDt) AS TrdExctnDtEOM,
-			SUM(EntrdVolQt) AS Volume
-		FROM
-			Trace_filteredWithRatings
+			IssuerId,
+			TrdExctnDtEOM
+		FROM (
+			SELECT
+				*,
+				DENSE_RANK() OVER (PARTITION BY IssuerId, TrdExctnDtEOM ORDER BY Volume DESC) AS VolumeRanking
+			FROM (
+				SELECT
+					IssuerId,
+					CusipId,
+					EOMONTH(TrdExctnDt) AS TrdExctnDtEOM,
+					SUM(EntrdVolQt) AS Volume
+				FROM
+					Trace_filteredWithRatings
+				WHERE
+					RatingNum <> 0
+					AND EntrdVolQt >= 500000 -- institunional
+					AND PrincipalAmt IS NOT NULL
+					AND TrdExctnDt <= EOMONTH(TrdExctnDt) AND TrdExctnDt > DATEADD(DAY, -5, EOMONTH(TrdExctnDt))
+				GROUP BY
+					IssuerId,
+					CusipId,
+					EOMONTH(TrdExctnDt)
+			) A
+		) B
 		WHERE
-			RatingNum <> 0
-			AND EntrdVolQt >= 500000 -- institunional
-			AND PrincipalAmt IS NOT NULL
-			AND TrdExctnDt <= EOMONTH(TrdExctnDt) AND TrdExctnDt > DATEADD(DAY, -5, EOMONTH(TrdExctnDt))
-		GROUP BY
-			IssuerId,
-			CusipId,
-			EOMONTH(TrdExctnDt)
-	) A
-) B
-WHERE
-	VolumeRanking <= 3
+			VolumeRanking <= 3
+	) C
+) D
