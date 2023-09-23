dc903d67072b4ac8ddecc1799eb13d4a6c02e06e
sonqo <stratoskrp@gmail.com>
2023-09-23 15:26:57 +0300

    Corrections on robustness measures

--- 0-thesis/robustness-measures/query-amihud.sql
+++ 0-thesis/robustness-measures/query-amihud.sql
@@ -35,6 +35,48 @@ FROM (
 ORDER BY
     TrdExctnDt
 
+-- WEEKLY
+SELECT DISTINCT
+    TrdExctnDtSOW,
+    PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY Amihud) OVER (PARTITION BY TrdExctnDtSOW) * 1000000 AS MedianAmihud
+FROM (
+    SELECT
+        CusipId,
+        TrdExctnDtSOW,
+        ABS(RptdPr - LagRptdPr) / Volume AS Amihud
+    FROM (    
+        SELECT
+            A.CusipId,
+            A.TrdExctnDtSOW,
+            A.RptdPr,
+            LAG(A.RptdPr) OVER (PARTITION BY A.CusipId ORDER BY A.TrdExctnDtSOW) AS LagRptdPr,
+            B.Volume
+        FROM (
+            SELECT
+                *,
+                DATEADD(DAY, 1 - DATEPART(WEEKDAY, TrdExctnDt) + 1, TrdExctnDt) AS TrdExctnDtSOW
+            FROM 
+                Trace_filteredWithRatings
+        ) A
+        INNER JOIN (    
+            SELECT
+                CusipId,
+                DATEADD(DAY, 1 - DATEPART(WEEKDAY, TrdExctnDt) + 1, TrdExctnDt) AS TrdExctnDtSOW,
+                MAX(TrdExctnTm) AS CloseTime,
+                SUM(EntrdVolQt) AS Volume
+            FROM
+                Trace_filteredWithRatings
+            GROUP BY
+                CusipId,
+                DATEADD(DAY, 1 - DATEPART(WEEKDAY, TrdExctnDt) + 1, TrdExctnDt)
+        ) B ON A.CusipId = B.CusipId AND A.TrdExctnDtSOW = B.TrdExctnDtSOW AND A.TrdExctnTm = B.CloseTime
+        WHERE
+            B.Volume <> 0
+    ) C
+) D
+ORDER BY
+    TrdExctnDtSOW
+
 -- MONTHLY
 SELECT DISTINCT
     TrdExctnDtEOM,
@@ -76,3 +118,45 @@ FROM (
 ) D
 ORDER BY
     TrdExctnDtEOM
+
+-- YEARLY
+SELECT DISTINCT
+    TrdExctnDtYr,
+    PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY Amihud) OVER (PARTITION BY TrdExctnDtYr) * 1000000 AS MedianAmihud
+FROM (
+    SELECT
+        CusipId,
+        TrdExctnDtYr,
+        ABS(RptdPr - LagRptdPr) / Volume AS Amihud
+    FROM (    
+        SELECT
+            A.CusipId,
+            A.TrdExctnDtYr,
+            A.RptdPr,
+            LAG(A.RptdPr) OVER (PARTITION BY A.CusipId ORDER BY A.TrdExctnDtYr) AS LagRptdPr,
+            B.Volume
+        FROM (
+            SELECT
+                *,
+                YEAR(TrdExctnDt) AS TrdExctnDtYr
+            FROM 
+                Trace_filteredWithRatings
+        ) A
+        INNER JOIN (    
+            SELECT
+                CusipId,
+                YEAR(TrdExctnDt) AS TrdExctnDtYr,
+                MAX(TrdExctnTm) AS CloseTime,
+                SUM(EntrdVolQt) AS Volume
+            FROM
+                Trace_filteredWithRatings
+            GROUP BY
+                CusipId,
+                YEAR(TrdExctnDt)
+        ) B ON A.CusipId = B.CusipId AND A.TrdExctnDtYr = B.TrdExctnDtYr AND A.TrdExctnTm = B.CloseTime
+        WHERE
+            B.Volume <> 0
+    ) C
+) D
+ORDER BY
+    TrdExctnDtYr
--- 0-thesis/robustness-measures/query-informationAsymmetry.sql
+++ 0-thesis/robustness-measures/query-informationAsymmetry.sql
@@ -1,20 +1,240 @@
+-- DAILY
 SELECT
-    *,
-    Qt * EntrdVolQt AS QtVt,
-    LAG(RptdPr) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagRptPr
-FROM (    
-    SELECT
-        TrdExctnDt,
-        CusipId,
-        RptdPr,
-        EntrdVolQt,
-        RptSideCd,
-        CASE WHEN RptSideCd = 'B' THEN 1 WHEN RptSideCd = 'S' THEN -1 END AS Qt
-    FROM
-        Trace_filteredWithRatings
-    WHERE
-        CntraMpId = 'C'
-        AND EntrdVolQt <> 1000000 AND EntrdVolQt <> 5000000
-) A
+	CusipId,
+	TrdExctnDt,
+	RptdPr,
+	EntrdVolQt,
+	LagRptdPr,
+	RptdPr - LagRptdPr AS DPt,
+	Qt,
+	LagQt,
+	Qt - LagQt AS DQt,
+	QtVt,
+	LagQtVt,
+	QtVt - LagQtVt AS DQtVt
+FROM (
+	SELECT
+		*,
+		LAG(RptdPr) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagRptdPr,
+		LAG(Qt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagQt,
+		LAG(QtVt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagQtVt,
+		DATEDIFF(DAY, LAG(TrdExctnDt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt), TrdExctnDt) AS TimeId
+	FROM (
+		SELECT
+			A.CusipId,
+			A.TrdExctnDt,
+			A.RptdPr,
+			A.EntrdVolQt,
+			A.RptSideCd,
+			CASE WHEN RptSideCd = 'S' THEN 1 WHEN RptSideCd = 'B' THEN -1 END AS Qt,
+			CASE WHEN RptSideCd = 'S' THEN 1 * EntrdVolQt WHEN RptSideCd = 'B' THEN -1 * EntrdVolQt END AS QtVt
+		FROM
+			Trace_filteredWithRatings A
+		INNER JOIN (
+			SELECT
+				CusipId,
+				TrdExctnDt,
+				MIN(TrdExctnTm) AS MinTrdExctnTm
+			FROM
+				Trace_filteredWithRatings
+			WHERE
+				CntraMpId = 'C'
+				AND EntrdVolQt <> CASE WHEN RatingNum <= 10 THEN 5000000 WHEN RatingNum >= 11 THEN 1000000 END
+			GROUP BY
+				CusipId,
+				TrdExctnDt
+		) B ON A.CusipId = B.CusipId AND A.TrdExctnDt = B.TrdExctnDt AND A.TrdExctnTm = B.MinTrdExctnTm
+	) A
+) B
 WHERE
-    DATEDIFF(DAY, LAG(TrdExctnDt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt), TrdExctnDt) = 1
+	TimeId = 1
+	AND	Qt - LagQt IS NOT NULL
+	AND QtVt - LagQtVt IS NOT NULL
+	AND RptdPr - LagRptdPr IS NOT NULL
+ORDER BY
+	TrdExctnDt,
+	CusipId
+
+-- WEEKLY
+SELECT
+	CusipId,
+	DATEADD(DAY, 1 - DATEPART(WEEKDAY, TrdExctnDt) + 1, TrdExctnDt) AS TrdExctnDtSOW,
+	RptdPr,
+	EntrdVolQt,
+	LagRptdPr,
+	RptdPr - LagRptdPr AS DPt,
+	Qt,
+	LagQt,
+	Qt - LagQt AS DQt,
+	QtVt,
+	LagQtVt,
+	QtVt - LagQtVt AS DQtVt
+FROM (
+	SELECT
+		*,
+		LAG(RptdPr) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagRptdPr,
+		LAG(Qt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagQt,
+		LAG(QtVt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagQtVt,
+		DATEDIFF(WEEK, LAG(TrdExctnDt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt), TrdExctnDt) AS TimeId
+	FROM (
+		SELECT
+			A.CusipId,
+			A.TrdExctnDt,
+			A.RptdPr,
+			A.EntrdVolQt,
+			A.RptSideCd,
+			CASE WHEN RptSideCd = 'S' THEN 1 WHEN RptSideCd = 'B' THEN -1 END AS Qt,
+			CASE WHEN RptSideCd = 'S' THEN 1 * EntrdVolQt WHEN RptSideCd = 'B' THEN -1 * EntrdVolQt END AS QtVt
+		FROM
+			Trace_filteredWithRatings A
+		INNER JOIN (
+			SELECT
+				CusipId,
+				TrdExctnDtSOW,
+				MIN(TrdExctnDt) AS MinTrdExctnDt,
+				MIN(TrdExctnTm) AS MinTrdExctnTm
+			FROM (
+				SELECT
+					*,
+					DATEADD(DAY, 1 - DATEPART(WEEKDAY, TrdExctnDt) + 1, TrdExctnDt) AS TrdExctnDtSOW
+				FROM
+					Trace_filteredWithRatings
+			) A
+			WHERE
+				CntraMpId = 'C'
+				AND EntrdVolQt <> CASE WHEN RatingNum <= 10 THEN 5000000 WHEN RatingNum >= 11 THEN 1000000 END
+			GROUP BY
+				CusipId,
+				TrdExctnDtSOW
+		) B ON A.CusipId = B.CusipId AND A.TrdExctnDt = B.MinTrdExctnDt AND A.TrdExctnTm = B.MinTrdExctnTm
+	) A
+) B
+WHERE
+	TimeId = 1
+	AND	Qt - LagQt IS NOT NULL
+	AND QtVt - LagQtVt IS NOT NULL
+	AND RptdPr - LagRptdPr IS NOT NULL
+ORDER BY
+	TrdExctnDt,
+	CusipId
+
+-- MONTHLY
+SELECT
+	CusipId,
+	EOMONTH(TrdExctnDt) AS TrdExctnDtEOM,
+	RptdPr,
+	EntrdVolQt,
+	LagRptdPr,
+	RptdPr - LagRptdPr AS DPt,
+	Qt,
+	LagQt,
+	Qt - LagQt AS DQt,
+	QtVt,
+	LagQtVt,
+	QtVt - LagQtVt AS DQtVt
+FROM (
+	SELECT
+		*,
+		LAG(RptdPr) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagRptdPr,
+		LAG(Qt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagQt,
+		LAG(QtVt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagQtVt,
+		DATEDIFF(MONTH, LAG(TrdExctnDt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt), TrdExctnDt) AS TimeId
+	FROM (
+		SELECT
+			A.CusipId,
+			A.TrdExctnDt,
+			A.RptdPr,
+			A.EntrdVolQt,
+			A.RptSideCd,
+			CASE WHEN RptSideCd = 'S' THEN 1 WHEN RptSideCd = 'B' THEN -1 END AS Qt,
+			CASE WHEN RptSideCd = 'S' THEN 1 * EntrdVolQt WHEN RptSideCd = 'B' THEN -1 * EntrdVolQt END AS QtVt
+		FROM
+			Trace_filteredWithRatings A
+		INNER JOIN (
+			SELECT
+				CusipId,
+				TrdExctnDtEOM,
+				MIN(TrdExctnDt) AS MinTrdExctnDt,
+				MIN(TrdExctnTm) AS MinTrdExctnTm
+			FROM (
+				SELECT
+					*,
+					EOMONTH(TrdExctnDt) AS TrdExctnDtEOM
+				FROM
+					Trace_filteredWithRatings
+			) A
+			WHERE
+				CntraMpId = 'C'
+				AND EntrdVolQt <> CASE WHEN RatingNum <= 10 THEN 5000000 WHEN RatingNum >= 11 THEN 1000000 END
+			GROUP BY
+				CusipId,
+				TrdExctnDtEOM
+		) B ON A.CusipId = B.CusipId AND A.TrdExctnDt = B.MinTrdExctnDt AND A.TrdExctnTm = B.MinTrdExctnTm
+	) A
+) B
+WHERE
+	TimeId = 1
+	AND	Qt - LagQt IS NOT NULL
+	AND QtVt - LagQtVt IS NOT NULL
+	AND RptdPr - LagRptdPr IS NOT NULL
+ORDER BY
+	TrdExctnDt,
+	CusipId
+
+-- YEARLY
+SELECT
+	CusipId,
+	YEAR(TrdExctnDt) AS TrdExctnDtYr,
+	RptdPr,
+	EntrdVolQt,
+	LagRptdPr,
+	RptdPr - LagRptdPr AS DPt,
+	Qt,
+	LagQt,
+	Qt - LagQt AS DQt,
+	QtVt,
+	LagQtVt,
+	QtVt - LagQtVt AS DQtVt
+FROM (
+	SELECT
+		*,
+		LAG(RptdPr) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagRptdPr,
+		LAG(Qt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagQt,
+		LAG(QtVt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt) AS LagQtVt,
+		DATEDIFF(YEAR, LAG(TrdExctnDt) OVER (PARTITION BY CusipId ORDER BY TrdExctnDt), TrdExctnDt) AS TimeId
+	FROM (
+		SELECT
+			A.CusipId,
+			A.TrdExctnDt,
+			A.RptdPr,
+			A.EntrdVolQt,
+			A.RptSideCd,
+			CASE WHEN RptSideCd = 'S' THEN 1 WHEN RptSideCd = 'B' THEN -1 END AS Qt,
+			CASE WHEN RptSideCd = 'S' THEN 1 * EntrdVolQt WHEN RptSideCd = 'B' THEN -1 * EntrdVolQt END AS QtVt
+		FROM
+			Trace_filteredWithRatings A
+		INNER JOIN (
+			SELECT
+				CusipId,
+				YEAR(TrdExctnDt) AS TrdExctnDtYr,
+				MIN(TrdExctnDt) AS MinTrdExctnDt,
+				MIN(TrdExctnTm) AS MinTrdExctnTm
+			FROM 
+				Trace_filteredWithRatings
+			WHERE
+				CntraMpId = 'C'
+				AND EntrdVolQt <> CASE WHEN RatingNum <= 10 THEN 5000000 WHEN RatingNum >= 11 THEN 1000000 END
+			GROUP BY
+				CusipId,
+				YEAR(TrdExctnDt)
+		) B ON A.CusipId = B.CusipId AND A.TrdExctnDt = B.MinTrdExctnDt AND A.TrdExctnTm = B.MinTrdExctnTm
+	) A
+) B
+WHERE
+	TimeId = 1
+	AND	Qt - LagQt IS NOT NULL
+	AND QtVt - LagQtVt IS NOT NULL
+	AND RptdPr - LagRptdPr IS NOT NULL
+ORDER BY
+	TrdExctnDt,
+	CusipId
